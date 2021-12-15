f70b0f706fc09c76c9cc6004271e55201f078d85
AnnaAleynik <anna.mikhailovna305@gmail.com>
2021-12-15 12:09:28 +0300

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-5.times do |i|
+20.times do |i|
   Card.create(title: "Card ##{i}")
 end
 
@@ -6,6 +6,6 @@ end
   Task.create(title: "Task - #{i}", card_id: 1)
 end
 
-2.times do |i|
-  Task.create(title: "Another", card_id: i + 2)
+20.times do |i|
+  Task.create(title: "Another", card_id: i)
 end
