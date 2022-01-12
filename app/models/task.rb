a78c4c0c2380daf94bd0d00380a42bd362c3c5ac
AnnaAleynik <anna.mikhailovna305@gmail.com>
2022-01-12 18:34:08 +0300

    fix rubocop config

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   belongs_to :card
   validates :title, presence: true, length: { minimum: 2 }
 
-  scope :actual, -> { where('created_at > ?', Date.today - 1.month) }
+  scope :actual, -> { where("created_at > ?", Date.today - 1.month) }
   # 1.month, 1.day to show how it work
 
   before_save :upcase_title
